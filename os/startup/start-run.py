75ed17f2bd7c28ca56e98b580cb5dde5c9d53d04
huluobuo <HelloHuHi@outlook.com>
2025-01-28 12:35:46 +0800

    b

--- os/startup/start-run.py
+++ os/startup/start-run.py
@@ -6,7 +6,13 @@ import time
 # 开机动画
 os.system("start .\\os\\startup\\start-hello.py" if os.name == "nt" else "python ./os/startup/start-hello.py")
 os.system("start .\\os\\startup\\startup.py" if os.name == "nt" else "python ./os/startup/startup.py")
-time.sleep(2)
+try:
+    with open("./os/mos/systemstartwaittime.mos", "r") as f:
+        waittime = int(f.read())
+except:
+    waittime = 3
+
+time.sleep(waittime)
 
 with open("./os/mos/systemisready.mos", "w") as f:
     f.write("True")
--- os/wsh/README.md
+++ os/wsh/README.md
@@ -3,3 +3,4 @@
 ## List
 * | Name | Description |
 * | [systemisready.mos](./systemisready.mos) | 系统是否就绪 |
+* | [systemstartwaittime.mos](./systemstartwaittime.mos) | 系统启动等待时间（int） |
