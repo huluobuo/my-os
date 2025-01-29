a8e9f52378afc4209e1d3cac6f2f60a51f0e3fe0
huluobuo <HelloHuHi@outlook.com>
2025-01-29 18:00:10 +0800

    aaaa

---
+++ home/program-files/system-update/system_update.py
@@ -0,0 +1,4 @@
+import os
+
+os.system("start https://github.com/huluobuo/my-os-history")
+print("自己看着办")
--- run.py
+++ run.py
@@ -3,4 +3,4 @@ import os
 print("欢迎使用OS")
 print("Checking the third-party software...")
 os.system("python -m pip install -r requirements.txt")
-os.system("python .\\os\\startup\\start-run.py" if os.name == "nt" else "python ./os/startup/start-run.py")
+os.system("start .\\os\\startup\\start-run.py" if os.name == "nt" else "python ./os/startup/start-run.py")
